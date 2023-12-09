51fd418fb677efa71549d7aa2dcb918b70f4e590
Edward Toroshchyn <dev@hades.name>
2023-12-09 06:18:26 +0100

    solve day 9

--- aoc23/day09.py
+++ aoc23/day09.py
@@ -1,10 +1,29 @@
 from .solver import Solver
-
-# pylint: disable=abstract-method
-
 class Day09(Solver):
 
   def __init__(self):
     super().__init__(9)
+    self.numbers: list[list[int]] = []
+
+  def presolve(self, input: str):
+    lines = input.rstrip().split('\n')
+    self.numbers = [[int(n) for n in line.split(' ')] for line in lines]
+    for line in self.numbers:
+      stack = [line]
+      while not all(x == 0 for x in stack[-1]):
+        diff = [stack[-1][i+1] - stack[-1][i] for i in range(len(stack[-1]) - 1)]
+        stack.append(diff)
+      stack.reverse()
+      stack[0].append(0)
+      stack[0].insert(0, 0)
+      for i in range(1, len(stack)):
+        stack[i].append(stack[i-1][-1] + stack[i][-1])
+        stack[i].insert(0, stack[i][0] - stack[i-1][0])
+
+  def solve_first_star(self) -> int:
+    return sum(line[-1] for line in self.numbers)
+
+  def solve_second_star(self) -> int:
+    return sum(line[0] for line in self.numbers)
 
 # vim: ts=2:sw=2:et
--- tests/test_day09.py
+++ tests/test_day09.py
@@ -1,6 +1,20 @@
 from unittest import TestCase
 
+from aoc23.day09 import Day09
+
+INPUT="""0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
 class TestDay09(TestCase):
-  pass
+  def test_first_star(self):
+    solver = Day09()
+    solver.presolve(INPUT)
+    self.assertEqual(114, solver.solve_first_star())
+
+  def test_second_star(self):
+    solver = Day09()
+    solver.presolve(INPUT)
+    self.assertEqual(2, solver.solve_second_star())
 
 # vim: ts=2:sw=2:et
