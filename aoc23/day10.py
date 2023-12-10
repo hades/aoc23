b8b009b221fb03117c567af872765c1d3877f42f
Edward Toroshchyn <dev@hades.name>
2023-12-10 07:31:41 +0100

    solve day 10

--- aoc23/day10.py
+++ aoc23/day10.py
@@ -1,10 +1,103 @@
 from .solver import Solver
 
-# pylint: disable=abstract-method
+_EXITS_MAP = {
+  '|': ((0, -1), (0, 1)),
+  '-': ((-1, 0), (1, 0)),
+  'L': ((1, 0), (0, -1)),
+  'J': ((-1, 0), (0, -1)),
+  '7': ((-1, 0), (0, 1)),
+  'F': ((1, 0), (0, 1)),
+  '.': (),
+  'S': (),
+}
 
 class Day10(Solver):
 
   def __init__(self):
     super().__init__(10)
+    self.maze: dict[tuple[int, int], str] = {}
+    self.start: tuple[int, int] = (0, 0)
+    self.dists: dict[tuple[int, int], int] = {}
+
+  def _pipe_has_exit(self, x: int, y: int, di: int, dj: int, inverse: bool = False) -> bool:
+    if inverse:
+      di, dj = -di, -dj
+    return (di, dj) in _EXITS_MAP[self.maze[(x, y)]]
+
+  def presolve(self, input: str):
+    self.maze: dict[tuple[int, int], str] = {}
+    self.start: tuple[int, int] = (0, 0)
+    for y, line in enumerate(input.rstrip().split('\n')):
+      for x, c in enumerate(line):
+        self.maze[(x, y)] = c
+        if c == 'S':
+          self.start = (x, y)
+    next_pos: list[tuple[int, int]] = []
+    directions_from_start = []
+    for di, dj in ((0, -1), (1, 0), (0, 1), (-1, 0)):
+      x, y = self.start[0] + di, self.start[1] + dj
+      if (x, y) not in self.maze:
+        continue
+      if not self._pipe_has_exit(x, y, di, dj, inverse=True):
+        continue
+      next_pos.append((x, y))
+      directions_from_start.append((di, dj))
+    self.maze[self.start] = [c for c, dmap in _EXITS_MAP.items()
+                              if set(directions_from_start) == set(dmap)][0]
+    dists: dict[tuple[int, int], int] = {}
+    cur_dist = 0
+    while True:
+      cur_dist += 1
+      new_next_pos = []
+      for x, y in next_pos:
+        if (x, y) in dists:
+          continue
+        dists[(x, y)] = cur_dist
+        for di, dj in ((0, -1), (1, 0), (0, 1), (-1, 0)):
+          nx, ny = x + di, y + dj
+          if (nx, ny) not in self.maze:
+            continue
+          if not self._pipe_has_exit(x, y, di, dj):
+            continue
+          new_next_pos.append((nx, ny))
+      if not new_next_pos:
+        break
+      next_pos = new_next_pos
+    self.dists = dists
+
+  def solve_first_star(self) -> int:
+    return max(self.dists.values())
+
+  def solve_second_star(self) -> int:
+    area = 0
+    for y in range(max(y for _, y in self.dists.keys()) + 1):
+      internal = False
+      previous_wall = False
+      wall_start_symbol = None
+      for x in range(max(x for x, _ in self.dists.keys()) + 1):
+        is_wall = (x, y) == self.start or (x, y) in self.dists
+        wall_continues = is_wall
+        pipe_type = self.maze[(x, y)]
+        if is_wall and pipe_type == '|':
+          internal = not internal
+          wall_continues = False
+        elif is_wall and not previous_wall and pipe_type in 'FL':
+          wall_start_symbol = pipe_type
+        elif is_wall and not previous_wall:
+          raise RuntimeError(f'expecting wall F or L at {x}, {y}, got {pipe_type}')
+        elif is_wall and previous_wall and pipe_type == 'J':
+          wall_continues = False
+          if wall_start_symbol == 'F':
+            internal = not internal
+        elif is_wall and previous_wall and pipe_type == '7':
+          wall_continues = False
+          if wall_start_symbol == 'L':
+            internal = not internal
+        elif not is_wall and previous_wall:
+          raise RuntimeError(f'expecting wall J or 7 at {x}, {y}, got {pipe_type}')
+        if internal and not is_wall:
+          area += 1
+        previous_wall = wall_continues
+    return area
 
 # vim: ts=2:sw=2:et
--- tests/test_day10.py
+++ tests/test_day10.py
@@ -1,7 +1,75 @@
 from unittest import TestCase
 
+from aoc23.day10 import Day10
+
+INPUT1=""".....
+.S-7.
+.|.|.
+.L-J.
+....."""
+
+INPUT2="""-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF"""
+
+INPUT3="""7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ"""
+
+INPUT4="""...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+..........."""
+
+INPUT5=""".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ..."""
 
 class TestDay10(TestCase):
-  pass
+  def test_input_1(self):
+    solver = Day10()
+    solver.presolve(INPUT1)
+    self.assertEqual(4, solver.solve_first_star())
+    self.assertEqual(1, solver.solve_second_star())
+
+  def test_input_2(self):
+    solver = Day10()
+    solver.presolve(INPUT2)
+    self.assertEqual(4, solver.solve_first_star())
+    self.assertEqual(1, solver.solve_second_star())
+
+  def test_input_3(self):
+    solver = Day10()
+    solver.presolve(INPUT3)
+    self.assertEqual(8, solver.solve_first_star())
+    self.assertEqual(1, solver.solve_second_star())
+
+  def test_input_4(self):
+    solver = Day10()
+    solver.presolve(INPUT4)
+    self.assertEqual(23, solver.solve_first_star())
+    self.assertEqual(4, solver.solve_second_star())
+
+  def test_input_5(self):
+    solver = Day10()
+    solver.presolve(INPUT5)
+    self.assertEqual(70, solver.solve_first_star())
+    self.assertEqual(8, solver.solve_second_star())
 
 # vim: ts=2:sw=2:et
