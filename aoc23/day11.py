0147c477de96613d570e638e6be1a6cc6563963f
Edward Toroshchyn <dev@hades.name>
2023-12-11 06:43:45 +0100

    solve day11

--- aoc23/day11.py
+++ aoc23/day11.py
@@ -1,10 +1,49 @@
 from .solver import Solver
 
-# pylint: disable=abstract-method
 
 class Day11(Solver):
 
   def __init__(self):
     super().__init__(11)
+    self.galaxies: list = []
+    self.blank_x: set[int] = set()
+    self.blank_y: set[int] = set()
+
+  def presolve(self, input: str):
+    lines = input.rstrip().split('\n')
+    self.galaxies = []
+    max_x = 0
+    max_y = 0
+    for y, line in enumerate(lines):
+      for x, c in enumerate(line):
+        if c == '#':
+          self.galaxies.append((x, y))
+        max_x = max(max_x, x)
+      max_y = max(max_y, y)
+    self.blank_x = set(range(max_x + 1)) - {x for x, _ in self.galaxies}
+    self.blank_y = set(range(max_y + 1)) - {y for _, y in self.galaxies}
+
+  def solve(self, expansion_factor: int) -> int:
+    galaxies = list(self.galaxies)
+    total = 0
+    for i in range(len(galaxies)):
+      for j in range(i + 1, len(galaxies)):
+        sx, sy = galaxies[i]
+        dx, dy = galaxies[j]
+        if sx > dx:
+          sx, dx = dx, sx
+        if sy > dy:
+          sy, dy = dy, sy
+        dist = sum((dx - sx, dy - sy,
+            max(0, expansion_factor - 1) * len([x for x in self.blank_x if sx < x < dx]),
+            max(0, expansion_factor - 1) * len([y for y in self.blank_y if sy < y < dy])))
+        total += dist
+    return total
+
+  def solve_first_star(self):
+    return self.solve(2)
+
+  def solve_second_star(self):
+    return self.solve(1000000)
 
 # vim: ts=2:sw=2:et
--- tests/test_day11.py
+++ tests/test_day11.py
@@ -1,7 +1,29 @@
 from unittest import TestCase
 
+from aoc23.day11 import Day11
+
+INPUT="""...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."""
 
 class TestDay11(TestCase):
-  pass
+  def test_first_star(self):
+    solver = Day11()
+    solver.presolve(INPUT)
+    self.assertEqual(374, solver.solve_first_star())
+
+  def test_second_star(self):
+    solver = Day11()
+    solver.presolve(INPUT)
+    self.assertEqual(1030, solver.solve(10))
+    self.assertEqual(8410, solver.solve(100))
+    self.assertEqual(82000210, solver.solve_second_star())
 
 # vim: ts=2:sw=2:et
