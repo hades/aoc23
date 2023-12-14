b0af13b4fa44595120167bc220d84aa9e4e90f9a
Edward Toroshchyn <dev@hades.name>
2023-12-14 10:01:19 +0100

    solve day 14

--- aoc23/day14.py
+++ aoc23/day14.py
@@ -1,10 +1,62 @@
+import numpy as np
+
 from .solver import Solver
 
-# pylint: disable=abstract-method
+
+def _tilt(row: list[int], reverse: bool = False) -> list[int]:
+  res = row[::-1] if reverse else row[:]
+  rock_x = 0
+  for x, item in enumerate(res):
+    if item == 1:
+      rock_x = x + 1
+    if item == 2:
+      if rock_x < x:
+        res[rock_x] = 2
+        res[x] = 0
+      rock_x += 1
+  return res[::-1] if reverse else res
 
 class Day14(Solver):
+  data: np.ndarray
 
   def __init__(self):
     super().__init__(14)
+
+  def presolve(self, input: str):
+    lines = input.splitlines()
+    self.data = np.zeros((len(lines), len(lines[0])), dtype=np.int8)
+    for x, line in enumerate(lines):
+      for y, char in enumerate(line):
+        if char == '#':
+          self.data[x, y] = 1
+        elif char == 'O':
+          self.data[x, y] = 2
+
+  def solve_first_star(self) -> int:
+    for y in range(self.data.shape[1]):
+      self.data[:, y] = _tilt(self.data[:, y].tolist())
+    return sum((self.data.shape[0] - x) * (self.data[x] == 2).sum() for x in range(self.data.shape[0]))
+
+  def solve_second_star(self) -> int:
+    seen = {}
+    order = []
+    for i in range(1000000000):
+      order += [self.data.copy()]
+      s = self.data.tobytes()
+      if s in seen:
+        loop_size = i - seen[s]
+        remainder = (1000000000 - i) % loop_size
+        self.data = order[seen[s] + remainder]
+        break
+      seen[s] = i
+      for y in range(self.data.shape[1]):
+        self.data[:, y] = _tilt(self.data[:, y].tolist())
+      for x in range(self.data.shape[0]):
+        self.data[x, :] = _tilt(self.data[x, :].tolist())
+      for y in range(self.data.shape[1]):
+        self.data[:, y] = _tilt(self.data[:, y].tolist(), reverse=True)
+      for x in range(self.data.shape[0]):
+        self.data[x, :] = _tilt(self.data[x, :].tolist(), reverse=True)
+    return sum((self.data.shape[0] - x) * (self.data[x] == 2).sum() for x in range(self.data.shape[0]))
 
 # vim: ts=2:sw=2:et
--- tests/test_day14.py
+++ tests/test_day14.py
@@ -1,7 +1,27 @@
 from unittest import TestCase
 
+from aoc23.day14 import Day14
+
+INPUT="""O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#...."""
 
 class TestDay14(TestCase):
-  pass
+  def test_first_star(self):
+    solver = Day14()
+    solver.presolve(INPUT)
+    self.assertEqual(136, solver.solve_first_star())
+
+  def test_second_star(self):
+    solver = Day14()
+    solver.presolve(INPUT)
+    self.assertEqual(64, solver.solve_second_star())
 
 # vim: ts=2:sw=2:et
