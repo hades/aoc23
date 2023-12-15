b5e1543e66108c9853c4f23e686b5df8409c2494
Edward Toroshchyn <dev@hades.name>
2023-12-15 10:54:52 +0100

    solve day 15

--- aoc23/day15.py
+++ aoc23/day15.py
@@ -1,10 +1,46 @@
+import collections
+import re
+
 from .solver import Solver
 
-# pylint: disable=abstract-method
+
+def _hash(string: str) -> int:
+  result = 0
+  for c in string:
+    result = (result + ord(c)) * 17 % 256
+  return result
+
+def _assert_full_match(pattern: str, string: str):
+  m = re.fullmatch(pattern, string)
+  if not m:
+    raise RuntimeError(f'pattern {pattern} does not match {string}')
+  return m
 
 class Day15(Solver):
+  input: list[str]
 
   def __init__(self):
     super().__init__(15)
+
+  def presolve(self, input: str):
+    self.input = input.rstrip().split(',')
+
+  def solve_first_star(self) -> int:
+    return sum(_hash(string) for string in self.input)
+
+  def solve_second_star(self) -> int:
+    boxes = [collections.OrderedDict() for _ in range(256)]
+    for instruction in self.input:
+      label, op, value = _assert_full_match(r'([a-z]+)([=-])(\d*)', instruction).groups()
+      box = boxes[_hash(label)]
+      match op:
+        case '-':
+          if label in box:
+            del box[label]
+        case '=':
+          box[label] = value
+    return sum((1 + box_idx) * (1 + lens_idx) * int(value)
+                for box_idx, box in enumerate(boxes)
+                for lens_idx, (_, value) in enumerate(box.items()))
 
 # vim: ts=2:sw=2:et
--- tests/test_day15.py
+++ tests/test_day15.py
@@ -1,7 +1,17 @@
 from unittest import TestCase
 
+from aoc23.day15 import Day15
+
 
 class TestDay15(TestCase):
-  pass
+  def test_first_star(self):
+    solver = Day15()
+    solver.presolve('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7')
+    self.assertEqual(solver.solve_first_star(), 1320)
+
+  def test_second_star(self):
+    solver = Day15()
+    solver.presolve('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7')
+    self.assertEqual(solver.solve_second_star(), 145)
 
 # vim: ts=2:sw=2:et
