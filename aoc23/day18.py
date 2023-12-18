29a5103744ece399e025e53a4a80bcdbf235bd5b
Edward Toroshchyn <dev@hades.name>
2023-12-18 12:57:30 +0100

    solve day 18

--- aoc23/day18.py
+++ aoc23/day18.py
@@ -1,10 +1,45 @@
 from .solver import Solver
 
-# pylint: disable=abstract-method
 
 class Day18(Solver):
 
   def __init__(self):
     super().__init__(18)
+
+  def presolve(self, input: str):
+    self.lines = input.splitlines()
+
+  def solve_first_star(self):
+    commands = []
+    for line in self.lines:
+      direction, distance, *_ = line.split(' ')
+      commands.append((direction, int(distance)))
+    return self._solve(commands)
+
+  def solve_second_star(self):
+    commands = []
+    for line in self.lines:
+      _, _, command = line.split(' ')
+      distance = int(command[2:-2], 16)
+      direction = ('R', 'D', 'L', 'U')[int(command[-2])]
+      commands.append((direction, distance))
+    return self._solve(commands)
+
+  def _solve(self, commands: list[tuple[str, int]]):
+    points: list[tuple[int, int]] = [(0, 0)]
+    perimeter_integer_points = 1
+    x, y = 0, 0
+    for direction, distance in commands:
+      dx, dy = {'R': (1, 0), 'L': (-1, 0), 'U': (0, -1), 'D': (0, 1)}[direction]
+      x, y = x + dx * distance, y + dy * distance
+      perimeter_integer_points += distance
+      points.append((x, y))
+    last_x, last_y = points[-1]
+    perimeter_integer_points += abs(last_x) + abs(last_y) - 1
+    area_x2 = sum((points[i][1] + points[(i+1) % len(points)][1]) *
+                  (points[i][0] - points[(i+1) % len(points)][0])
+                  for i in range(len(points)))
+    interior_integer_points = (area_x2 - perimeter_integer_points) // 2 + 1
+    return interior_integer_points + perimeter_integer_points
 
 # vim: ts=2:sw=2:et
--- tests/test_day18.py
+++ tests/test_day18.py
@@ -1,7 +1,41 @@
 from unittest import TestCase
 
+from aoc23.day18 import Day18
+
+INPUT='''R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)
+'''
+
 
 class TestDay18(TestCase):
-  pass
+  def test_first_star(self):
+    solver = Day18()
+    solver.presolve(INPUT)
+    self.assertEqual(solver.solve_first_star(), 62)
+
+  def test_second_star(self):
+    solver = Day18()
+    solver.presolve(INPUT)
+    self.assertEqual(solver.solve_second_star(), 952408144115)
+
+  def test_second_star_simple(self):
+    solver = Day18()
+    solver.presolve('''_ _ (#000020)
+_ _ (#000021)
+_ _ (#000022)
+_ _ (#000013)''')
+    self.assertEqual(solver.solve_second_star(), 9)
 
 # vim: ts=2:sw=2:et
