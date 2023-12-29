04e8790bce91cc055beb41df90589767d3739740
Edward Toroshchyn <dev@hades.name>
2023-12-29 07:07:40 +0100

    solve day 25

--- aoc23/day25.py
+++ aoc23/day25.py
@@ -1,10 +1,22 @@
+import networkx as nx
+
 from .solver import Solver
 
-# pylint: disable=abstract-method
 
 class Day25(Solver):
 
   def __init__(self):
     super().__init__(25)
+
+  def presolve(self, input: str):
+    self.graph = nx.Graph()
+    for line in input.splitlines():
+      from_, to_line = line.split(': ')
+      for to in to_line.split(' '):
+        self.graph.add_edge(from_, to)
+
+  def solve_first_star(self) -> int | str:
+    cut_value, partition = nx.algorithms.stoer_wagner(self.graph)
+    return len(partition[0]) * len(partition[1])
 
 # vim: ts=2:sw=2:et
--- tests/test_day25.py
+++ tests/test_day25.py
@@ -1,7 +1,26 @@
 from unittest import TestCase
 
+from aoc23.day25 import Day25
+
+INPUT='''jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr
+'''
 
 class TestDay25(TestCase):
-  pass
+  def test_first_star(self):
+    solver = Day25()
+    solver.presolve(INPUT)
+    self.assertEqual(54, solver.solve_first_star())
 
 # vim: ts=2:sw=2:et
